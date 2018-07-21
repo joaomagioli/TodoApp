8a3c235af6af6a408dc32ecf7d41164383d64509
Joao Guilherme <joaomagioli@gmail.com>
2018-07-21 18:33:00 -0300

    Layout to task list

--- app/src/main/java/com/example/android/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/android/todo_app/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
